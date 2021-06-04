05e993ee55124b854b69adf4a3377b8015d1a967
schufeli <dextter767@gmail.com>
2021-06-04 13:31:23 +0200

    Added Swagger UI

---
+++ api/Controllers/BaseApiController.cs
@@ -0,0 +1,45 @@
+﻿using api.Data;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        #region Constructor
+        public BaseApiController(
+            ApplicationDbContext context)
+        {
+            DbContext = context;
+
+            JsonSettings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented
+            };
+        }
+        #endregion
+
+        #region Methods
+        // return HTTP Status 500 if client pyload is invalid
+        public StatusCodeResult ValidateClientPayload(Object model)
+        {
+            if (model == null) return new StatusCodeResult(500);
+            else return new StatusCodeResult(200);
+        }
+
+        // return HTTP Status 404 if entity was not found
+        public StatusCodeResult ValidateEntityExistence(Object model)
+        {
+            if (model == null) return new StatusCodeResult(404);
+            else return new StatusCodeResult(200);
+        }
+        #endregion
+
+        #region Properties
+        protected ApplicationDbContext DbContext { get; private set; }
+        protected JsonSerializerSettings JsonSettings { get; private set; }
+        #endregion
+    }
+}
--- api/Startup.cs
+++ api/Startup.cs
@@ -1,11 +1,11 @@
 using api.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace api
 {
@@ -29,6 +29,12 @@ namespace api
             // Add MVC Controllers and NetwonsoftJson to them
             services.AddControllers()
                 .AddNewtonsoftJson();
+
+            // User Swagger for API documentation
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Values Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,9 +43,14 @@ namespace api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication1 v1"));
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
+
 
             app.UseEndpoints(endpoints =>
             {
